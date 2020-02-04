6e1a66e1a9ea287f4828edd9dd990123cfadd907
watson-sam <watson.sam95@gmail.com>
2020-02-04 11:20:32 +0000

    upgrade to 0.7.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="iso3_lookup",
-    version="0.6.0",
+    version="0.7.0",
     packages=find_packages(),
-    entry_points={"console_scripts": ["iso3_tests.py = iso3_lookup.tests.run"]},
+    entry_points={"console_scripts": ["iso3_tests.py = iso3_lookup.tests:run"]},
     install_requires=["fuzzywuzzy", "python-Levenshtein"],
     author="Sam Watson",
-    author_email="quadquants@gmail.com",
+    author_email="watson.sam95@gmail.com",
     description="Go from iso3 -> country name and visa versa",
     url="https://github.com/watson-sam/iso3_lookup",
 )
