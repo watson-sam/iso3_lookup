53d8d2a5a17a2dd239da44fd26057d33c842a888
watson-sam <watson.sam95@gmail.com>
2020-02-04 11:30:51 +0000

    add requests

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="iso3_lookup",
-    version="0.7.0",
+    version="0.8.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["iso3_tests.py = iso3_lookup.tests:run"]},
-    install_requires=["fuzzywuzzy", "python-Levenshtein"],
+    install_requires=["fuzzywuzzy", "python-Levenshtein", "requests"],
     author="Sam Watson",
     author_email="watson.sam95@gmail.com",
     description="Go from iso3 -> country name and visa versa",
