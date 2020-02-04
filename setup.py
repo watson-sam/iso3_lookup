d82cb07cedb30cce3c6ea6dc3682cb33ba804ca0
watson-sam <watson.sam95@gmail.com>
2020-02-04 11:39:19 +0000

    new vers code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iso3_lookup",
-    version="0.8.0",
+    version="0.9.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["iso3_tests.py = iso3_lookup.tests:run"]},
     install_requires=["fuzzywuzzy", "python-Levenshtein", "requests"],
